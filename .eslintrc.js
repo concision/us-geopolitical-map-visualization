0e4777fb4d9afa4d8c48875277fc94cad8d8d86d
Concision <contact@concision.me>
2020-08-09 18:51:55 -0700

    feat: Added and configured ESlint Installed eslint, @typescript-eslint/parser, and @typescript-eslint/eslint-plugin development dependencies Configured .eslintrc.js for TypeScript and JavaScript linting for browser Configured d3 and topojson libraries as linter globals

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// TypeScript configuration sourced from https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
+// configuration: https://eslint.org/docs/user-guide/configuring
+module.exports = {
+    root: true,
+    ignorePatterns: [
+        ".eslintrc.js",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        sourceType: "script",
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    env: {
+        browser: true,
+    },
+    globals: {
+        "d3": "readonly",
+        "topojson": "readonly",
+    },
+};
