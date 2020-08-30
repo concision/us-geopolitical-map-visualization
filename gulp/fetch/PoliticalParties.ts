dc06627ce6ffd440edd67d6c6392c0478d7426f7
Concision <contact@concision.me>
2020-08-30 11:06:50 -0700

    feat(gulp): Implemented fetch for ICPSR state codes

--- gulp/fetch/PoliticalParties.ts
+++ gulp/fetch/PoliticalParties.ts
@@ -17,6 +17,18 @@ export interface PartyCodeMap {
     [key: number]: string;
 }
 
+// state
+
+export interface StateCode {
+    readonly id: number;
+    readonly code: string;
+    readonly name: string;
+}
+
+export interface StateCodeMap {
+    [key: number]: Pick<StateCode, "code" | "name">;
+}
+
 // noinspection DuplicatedCode
 /**
  * Fetches state congress members' political affiliations from https://legacy.voteview.com/icpsr.htm
@@ -24,15 +36,27 @@ export interface PartyCodeMap {
 @Gulpclass()
 export class Gulpfile {
     /**
-     * Party codes to party names
+     * Resolves party codes to party names
      * @private
      */
     private static readonly PARTY_CODES_URL: string = "https://legacy.voteview.com/PARTY3.HTM";
     /**
-     *
+     * Cached party codes file
      * @private
      */
     private static readonly PARTY_CODES_FILE: string = resolve("data", "party_codes.json");
+
+    /**
+     * Resolves state codes to state names
+     * @private
+     */
+    private static readonly STATE_CODES_URL: string = "https://legacy.voteview.com/state_codes_icpsr.htm";
+    /**
+     * Cached state codes file
+     * @private
+     */
+    private static readonly STATE_CODES_FILE: string = resolve("data", "state_codes.json");
+
 
     @Task("fetch:party_codes")
     public async fetchPartyCodes(): Promise<void> {
@@ -94,5 +118,68 @@ export class Gulpfile {
 
         // cache sessions
         writeFileSync(Gulpfile.PARTY_CODES_FILE, JSON.stringify(partyMap));
+    }
+
+    @Task("fetch:state_codes")
+    public async fetchStateCodes(): Promise<void> {
+        // try cached data
+        if (existsSync(Gulpfile.STATE_CODES_FILE)) {
+            try {
+                const lastModified: Date = statSync(Gulpfile.STATE_CODES_FILE).mtime;
+                // check expiration
+                if (moment(lastModified).isBefore(moment().subtract(1, "month"))) {
+                    log.info("Ignoring state codes as it is older than a month");
+                    return;
+                }
+            } catch (error) {
+                log.error("Failed to read last modified time");
+                throw error;
+            }
+        }
+
+
+        // initialize request
+        log.info(`Fetching state codes from ${Gulpfile.STATE_CODES_URL}`);
+        let response: AxiosResponse<string>;
+        try {
+            response = await axios.get<string>(Gulpfile.STATE_CODES_URL, {responseType: "text"});
+        } catch (exception) {
+            log.error("Failed to fetch state codes");
+            throw exception;
+        }
+
+        // parse HTML data into a DOM model
+        const selector: CheerioStatic = load(response.data);
+        // select b tag, nested under a pre tag (there should only be one match);
+        const tag: Cheerio = selector("pre > b");
+        if (tag.length === 0)
+            throw new Error("state code tag not found");
+        const textContent: string | null = tag.text();
+        if (textContent === null)
+            throw new Error("state code tag has no body");
+        // parse codes
+        const states: StateCode[] = textContent.split(/[\r\n]+/)
+            //filter out empty lines
+            .filter((line: string): boolean => line.length !== 0)
+            // trim line from spaces
+            .map((line: string): string => line.trim())
+            .map((line: string): StateCode => {
+                const [id, code, ...name] = line.split(/\s+/);
+                return {
+                    id: parseInt(id),
+                    code: code,
+                    name: name.join(" "),
+                };
+            });
+
+        // index by id
+        const codeMap: StateCodeMap = {};
+        for (const code of states) {
+            codeMap[code.id] = {code: code.code, name: code.name};
+        }
+        log.info(`Fetched ${states.length} state code mappings`);
+
+        // cache sessions
+        writeFileSync(Gulpfile.STATE_CODES_FILE, JSON.stringify(codeMap));
     }
 }
